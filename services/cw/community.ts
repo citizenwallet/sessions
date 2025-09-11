d2aba452c4111111df99b83bb61de0ea815a1fdb
kev-techi <kevin@techi.be>
2025-09-11 13:44:19 +0200

    parse request correctly

--- services/cw/community.ts
+++ services/cw/community.ts
@@ -2,13 +2,19 @@ import 'server-only';
 
 import { Config } from '@citizenwallet/sdk';
 
+interface RCommunity {
+  json: Config;
+}
+
 export const getConfigOfAlias = async (alias: string): Promise<Config> => {
   const dashboardUrl = process.env.DASHBOARD_BASE_URL;
   if (!dashboardUrl) throw new Error('DASHBOARD_URL is not set');
 
-  const community: Config = await fetch(
+  const rCommunity: RCommunity = await fetch(
     `${dashboardUrl}/communities/${alias}`
   ).then((res) => res.json());
+
+  const community = rCommunity.json;
 
   if (!community) throw new Error(`Community ${alias} not found`);
 
