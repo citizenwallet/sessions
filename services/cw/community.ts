4f0be25ef9663612613cb5bde4e263ce220fc688
kev-techi <kevin@techi.be>
2025-09-11 13:19:26 +0200

    fetch configs from api

--- services/cw/community.ts
+++ services/cw/community.ts
@@ -1,12 +1,14 @@
 import 'server-only';
 
 import { Config } from '@citizenwallet/sdk';
-import communities from '@/services/cw/communities.json' assert { type: 'json' };
 
-export const getConfigOfAlias = (alias: string): Config => {
-  const community: Config = communities.find(
-    (community) => community.community.alias === alias
-  ) as unknown as Config;
+export const getConfigOfAlias = async (alias: string): Promise<Config> => {
+  const dashboardUrl = process.env.DASHBOARD_URL;
+  if (!dashboardUrl) throw new Error('DASHBOARD_URL is not set');
+
+  const community: Config = await fetch(
+    `${dashboardUrl}/communities/${alias}`
+  ).then((res) => res.json());
 
   if (!community) throw new Error(`Community ${alias} not found`);
 
